d03ba33fd7e133ee35ba96cca7b0ada9c98101e9
Trix <157342883+TrixSec@users.noreply.github.com>
2024-09-11 23:29:07 +0530

    Commit & Push

--- thirdparty/scraper.py
+++ thirdparty/scraper.py
@@ -1,56 +1,64 @@
 import scrapy
-from scrapy.linkextractors import LinkExtractor
-from scrapy.spiders import CrawlSpider, Rule
-from tempfile import NamedTemporaryFile
-import os
+import re
 
-# Extensions to exclude during scraping
-CRAWL_EXCLUDE_EXTENSIONS = ("3ds", "3g2", "3gp", "7z", "DS_Store", "a", "aac", "adp", "ai", "aif", 
-                            "aiff", "apk", "ar", "asf", "au", "avi", "bak", "bin", "bk", "bmp", 
-                            "btif", "bz2", "cab", "caf", "cgm", "cmx", "cpio", "cr2", "dat", 
-                            "deb", "djvu", "dll", "dmg", "dmp", "dng", "doc", "docx", "dot", 
-                            "dotx", "dra", "dsk", "dts", "dtshd", "dvb", "dwg", "dxf", "ear", 
-                            "ecelp4800", "ecelp7470", "ecelp9600", "egg", "eol", "eot", "epub", 
-                            "exe", "f4v", "fbs", "fh", "fla", "flac", "fli", "flv", "fpx", "fst", 
-                            "fvt", "g3", "gif", "gz", "h261", "h263", "h264", "ico", "ief", 
-                            "image", "img", "ipa", "iso", "jar", "jpeg", "jpg", "jpgv", "jpm", 
-                            "jxr", "ktx", "lvp", "lz", "lzma", "lzo", "m3u", "m4a", "m4v", "mar", 
-                            "mdi", "mid", "mj2", "mka", "mkv", "mmr", "mng", "mov", "movie", "mp3", 
-                            "mp4", "mp4a", "mpeg", "mpg", "mpga", "mxu", "nef", "npx", "o", "oga", 
-                            "ogg", "ogv", "otf", "pbm", "pcx", "pdf", "pea", "pgm", "pic", "png", 
-                            "pnm", "ppm", "pps", "ppt", "pptx", "ps", "psd", "pya", "pyc", "pyo", 
-                            "pyv", "qt", "rar", "ras", "raw", "rgb", "rip", "rlc", "rz", "s3m", 
-                            "s7z", "scm", "scpt", "sgi", "shar", "sil", "smv", "so", "sub", "swf", 
-                            "tar", "tbz2", "tga", "tgz", "tif", "tiff", "tlz", "ts", "ttf", "uvh", 
-                            "uvi", "uvm", "uvp", "uvs", "uvu", "viv", "vob", "war", "wav", "wax", 
-                            "wbmp", "wdp", "weba", "webm", "webp", "whl", "wm", "wma", "wmv", 
-                            "wmx", "woff", "woff2", "wvx", "xbm", "xif", "xls", "xlsx", "xlt", 
-                            "xm", "xpi", "xpm", "xwd", "xz", "z", "zip", "zipx")
+CRAWL_EXCLUDE_EXTENSIONS = (
+    "3ds", "3g2", "3gp", "7z", "DS_Store", "a", "aac", "adp", "ai", "aif", 
+    "aiff", "apk", "ar", "asf", "au", "avi", "bak", "bin", "bk", "bmp", 
+    "btif", "bz2", "cab", "caf", "cgm", "cmx", "cpio", "cr2", "dat", "deb", 
+    "djvu", "dll", "dmg", "dmp", "dng", "doc", "docx", "dot", "dotx", "dra", 
+    "dsk", "dts", "dtshd", "dvb", "dwg", "dxf", "ear", "ecelp4800", 
+    "ecelp7470", "ecelp9600", "egg", "eol", "eot", "epub", "exe", "f4v", 
+    "fbs", "fh", "fla", "flac", "fli", "flv", "fpx", "fst", "fvt", "g3", 
+    "gif", "gz", "h261", "h263", "h264", "ico", "ief", "image", "img", "ipa", 
+    "iso", "jar", "jpeg", "jpg", "jpgv", "jpm", "jxr", "ktx", "lvp", "lz", 
+    "lzma", "lzo", "m3u", "m4a", "m4v", "mar", "mdi", "mid", "mj2", "mka", 
+    "mkv", "mmr", "mng", "mov", "movie", "mp3", "mp4", "mp4a", "mpeg", "mpg", 
+    "mpga", "mxu", "nef", "npx", "o", "oga", "ogg", "ogv", "otf", "pbm", 
+    "pcx", "pdf", "pea", "pgm", "pic", "png", "pnm", "ppm", "pps", "ppt", 
+    "pptx", "ps", "psd", "pya", "pyc", "pyo", "pyv", "qt", "rar", "ras", 
+    "raw", "rgb", "rip", "rlc", "rz", "s3m", "s7z", "scm", "scpt", "sgi", 
+    "shar", "sil", "smv", "so", "sub", "swf", "tar", "tbz2", "tga", "tgz", 
+    "tif", "tiff", "tlz", "ts", "ttf", "uvh", "uvi", "uvm", "uvp", "uvs", 
+    "uvu", "viv", "vob", "war", "wav", "wax", "wbmp", "wdp", "weba", "webm", 
+    "webp", "whl", "wm", "wma", "wmv", "wmx", "woff", "woff2", "wvx", "xbm", 
+    "xif", "xls", "xlsx", "xlt", "xm", "xpi", "xpm", "xwd", "xz", "z", "zip", 
+    "zipx"
+)
 
-class MySpider(CrawlSpider):
-    name = 'waymap_spider'
-    allowed_domains = []  # Set the domain here
-    start_urls = []  # The URL to start crawling
+VALID_SYMBOLS = ["=", "&", "?", "'", '"', "(", ")", ";", "--", "/*", "*/", "+", "%", "\\"]
+
+def contains_valid_symbol(url):
+    """Check if the URL contains any of the valid symbols for filtering."""
+    for symbol in VALID_SYMBOLS:
+        if symbol in url:
+            return True
+    return False
+
+class UrlSpider(scrapy.Spider):
+    name = "url_spider"
     
-    rules = (
-        Rule(LinkExtractor(deny_extensions=CRAWL_EXCLUDE_EXTENSIONS), callback='parse_item', follow=True),
-    )
-
-    def __init__(self, *args, **kwargs):
-        self.temp_file = NamedTemporaryFile(delete=False, mode='w')
+    def __init__(self, domain, crawl_depth, output_file, *args, **kwargs):
         super().__init__(*args, **kwargs)
+        self.start_urls = [domain]
+        self.crawl_depth = crawl_depth
+        self.output_file = output_file
 
-    def parse_item(self, response):
-        url = response.url
-        # Write to tempfile if not already present
-        if url not in self.temp_file.read():
-            self.temp_file.write(url + "\n")
-            print(f"[*] URL saved: {url}")
+    def parse(self, response):
+        current_depth = response.meta.get('depth', 1)
 
-    def close(self, reason):
-        # Close the temp file when scraping is done
-        self.temp_file.close()
+        # Save valid URLs only
+        with open(self.output_file, 'a') as f:
+            for link in response.css('a::attr(href)').getall():
+                absolute_url = response.urljoin(link)
+                
+                # Filter out media links and check for valid symbols
+                if not absolute_url.endswith(CRAWL_EXCLUDE_EXTENSIONS) and contains_valid_symbol(absolute_url):
+                    f.write(absolute_url + "\n")
 
-# Utility function to run the scrapy spider
-def run_scrapy():
-    os.system('scrapy crawl waymap_spider')
+        # Recursively follow links up to the crawl depth
+        if current_depth < self.crawl_depth:
+            for link in response.css('a::attr(href)').getall():
+                next_page = response.urljoin(link)
+                if next_page is not None:
+                    yield scrapy.Request(next_page, callback=self.parse, meta={'depth': current_depth + 1})
+
