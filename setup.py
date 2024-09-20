013f7f53c143434db66625964acf61fe0d280df1
Trix Cyrus <157342883+TrixSec@users.noreply.github.com>
2024-09-21 00:18:50 +0530

    final

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ setup(
     },
     classifiers=[
         'Programming Language :: Python :: 3',
-        'License :: OSI Approved :: MIT License',
+        'License :: OSI Approved :: GPL-3.0 license',
         'Operating System :: OS Independent',
     ],
     python_requires='>=3.6',  # Update if you need a specific Python version
