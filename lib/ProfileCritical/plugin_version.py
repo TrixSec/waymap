cc480e3ede171de994528508e904352a95f4e1ec
Trix Cyrus <157342883+TrixSec@users.noreply.github.com>
2024-11-16 21:56:38 +0530

    v5.4.1

--- lib/ProfileCritical/plugin_version.py
+++ lib/ProfileCritical/plugin_version.py
@@ -20,39 +20,37 @@ def get_plugin_version_urls(plugin_name):
 def detect_plugin_version(target_url, plugin_name):
     """
     Fetches version details from the plugin's relevant files like readme.txt, changelog.txt, and changelog.md.
-    Returns the detected version or None if not found.
+    Returns the detected version or a message if not found.
     """
     try:
         urls_to_check = get_plugin_version_urls(plugin_name)
-        failed_urls = [] 
-        
+        all_404 = True 
+
         for url_path in urls_to_check:
             full_url = urljoin(target_url, url_path)
             response = requests.get(full_url, timeout=10, verify=False)
+            
             if response.status_code == 200:
                 if "Version:" in response.text:
                     version_line = next(line for line in response.text.splitlines() if "Version:" in line)
                     version = version_line.split(":")[1].strip()
                     return version
-                
+
                 elif "Stable tag:" in response.text:
                     version_line = next(line for line in response.text.splitlines() if "Stable tag:" in line)
                     version = version_line.split(":")[1].strip()
                     return version
-                
+
                 elif "#" in response.text:
                     lines = response.text.splitlines()
                     for line in lines:
                         if line.startswith("####"):
                             version = line.split()[1]
                             return version
-            else:
-                failed_urls.append(full_url)
-        
-        if len(failed_urls) == len(urls_to_check):
-            print(f"Plugin '{plugin_name}' not found at {target_url}. All URLs failed.")
-        
-        return None  
-    except requests.exceptions.RequestException as e:
-        return None
+                all_404 = False  
 
+        if all_404:
+            return f"Plugin '{plugin_name}' not found at {target_url}"
+
+    except requests.exceptions.RequestException:
+        return None
